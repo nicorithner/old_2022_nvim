4aa41bed9ae2d89e8f1363739497e28365a1ff11
Nico Rithner <nicorithner@gmail.com>
2022-04-17 12:59:35 -0600

    Initial commit. Add vim options

---
+++ init.lua
@@ -0,0 +1,38 @@
+-- installs packer if not already installed
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-- require plugins files
+require('nico.autopairs')
+require('nico.autosave')
+require('nico.bufferline')
+require('nico.colour')
+require('nico.colorizer')
+require('nico.commands')
+require('nico.comment')
+require('nico.dashboard')
+require('nico.gitsigns')
+require('nico.keymaps')
+require('nico.lualine')
+require('nico.lsp-cmp')
+require('nico.lsp-config')
+require('nico.lazygit')
+require('nico.nvim-tree')
+require('nico.options')
+require('nico.plugins')
+require('nico.treesitter')
+require('nico.toggleTerm')
+require('nico.toggle_lsp_diagnostics')
+require('nico.telescope')
+
+-- CpHelper config
+vim.g.cphdir = os.getenv( 'HOME' )..'/problems'
+
+-- Rooter config
+vim.g.rooter_pattern = { 'input1', 'output1' }
---
+++ lua/nico/options.lua
@@ -0,0 +1,62 @@
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = true,                            --  change to false display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd("highlight GitSignsCurrentLineBlame guifg=#545c7e")
+
+vim.g.markdown_fenced_languages = {'bash=sh', 'css', 'javascript', 'js=javascript', 'json=javascript', 'jsx=javascript','ruby', 'sass', 'xml', 'html'}
+
+-- mkdir $HOME/.vim/undo
+vim.opt['undofile'] = true
+vim.opt['undodir'] = os.getenv( 'HOME' )..'/.cache/nvim/undo'
+vim.opt['undolevels'] = 1000
+vim.opt['undoreload'] = 10000
+
+-- Stop comments on newline
+vim.cmd([[autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o]])
+
+-- Disable lualine on Nvim-tree
+vim.cmd ([[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == 'NvimTree' | set laststatus=0 | else | set laststatus=2 | endif]])
+
+vim.cmd('command CDD cd %:p:h')
