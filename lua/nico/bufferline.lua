83e189ad60ed263c99b57719c238b867e6f98bf2
Nico Rithner <nico@altvia.com>
2022-05-12 11:51:05 -0600

    Change width of tab

--- lua/nico/bufferline.lua
+++ lua/nico/bufferline.lua
@@ -25,9 +25,9 @@ require('bufferline').setup {
         return vim.fn.fnamemodify(buf.name, ':t:r')
       end
     end,
-    max_name_length = 18,
+    max_name_length = 30,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-    tab_size = 18,
+    tab_size = 30,
     diagnostics = false, -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
