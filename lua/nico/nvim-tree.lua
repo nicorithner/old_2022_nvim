5f679e1a74c1c4233f14792d2a68306b8b183e48
Nico Rithner <nico@altvia.com>
2022-05-12 11:51:19 -0600

    Add numbers to nvim-tree

--- lua/nico/nvim-tree.lua
+++ lua/nico/nvim-tree.lua
@@ -53,7 +53,7 @@ nvim_tree.setup({
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
-    number = false,
+    number = true,
     relativenumber = false,
     signcolumn = "yes",
     mappings = {
