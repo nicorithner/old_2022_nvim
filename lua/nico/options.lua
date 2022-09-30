f61234ddfc2560e5fcd2dd8336dc17b51fae0adc
Nico Rithner <nicorithner@gmail.com>
2022-09-30 09:34:57 -0600

    Update options

--- lua/nico/options.lua
+++ lua/nico/options.lua
@@ -1,62 +1,85 @@
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = false,                  -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,                            --  change to false display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+	title = true,
+	backup = false, -- creates a backup file
+	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+	cmdheight = 2, -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8", -- the encoding written to a file
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	mouse = "a", -- allow the mouse to be used in neovim
+	pumheight = 10, -- pop up menu height
+	winblend = 0,
+	wildoptions = "pum",
+	pumblend = 5,
+	background = "dark",
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 2, -- always show tabs
+	smartcase = true, -- smart case
+	autoindent = true,
+	smartindent = true, -- make indenting smarter again
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true, -- enable persistent undo
+	updatetime = 300, -- faster completion (4000ms default)
+	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	tabstop = 2, -- insert 2 spaces for a tab
+	cursorline = true, -- highlight the current line
+	number = true, -- set numbered lines
+	relativenumber = false, -- set relative numbered lines
+	numberwidth = 4, -- set number column width to 2 {default 4}
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	wrap = true, --  change to false display lines as one long line
+	scrolloff = 8, -- is one of my fav
+	sidescrolloff = 8,
+	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	laststatus = 2,
+	inccommand = "split",
+	backspace = { "start", "eol", "indent" },
 }
 
-vim.g.ruby_host_prog = 'rvm system do neovim-ruby-host'
-
-vim.opt.shortmess:append "c"
+vim.g.ruby_host_prog = "rvm system do neovim-ruby-host"
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.shortmess:append("c")
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
 vim.cmd("highlight GitSignsCurrentLineBlame guifg=#545c7e")
 
-vim.g.markdown_fenced_languages = {'bash=sh', 'css', 'javascript', 'js=javascript', 'json=javascript', 'jsx=javascript','ruby', 'sass', 'xml', 'html'}
+vim.g.markdown_fenced_languages = {
+	"bash=sh",
+	"css",
+	"javascript",
+	"js=javascript",
+	"json=javascript",
+	"jsx=javascript",
+	"ruby",
+	"sass",
+	"xml",
+	"html",
+}
 
 -- mkdir $HOME/.vim/undo
-vim.opt['undofile'] = true
-vim.opt['undodir'] = os.getenv( 'HOME' )..'/.cache/nvim/undo'
-vim.opt['undolevels'] = 1000
-vim.opt['undoreload'] = 10000
+vim.opt["undofile"] = true
+vim.opt["undodir"] = os.getenv("HOME") .. "/.cache/nvim/undo"
+vim.opt["undolevels"] = 1000
+vim.opt["undoreload"] = 10000
 
 -- Stop comments on newline
 vim.cmd([[autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o]])
 
 -- Disable lualine on Nvim-tree
-vim.cmd ([[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == 'NvimTree' | set laststatus=0 | else | set laststatus=2 | endif]])
+vim.cmd(
+	[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == 'NvimTree' | set laststatus=0 | else | set laststatus=2 | endif]]
+)
