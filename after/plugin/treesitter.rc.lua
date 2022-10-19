51c2cd6ca17b4a14edddc9da63bee84fb5bbf450
Nico Rithner <nicorithner@gmail.com>
2022-10-19 16:50:25 -0600

    Add filetypes for autotag

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -48,6 +48,24 @@ configs.setup({
 
 	autotag = {
 		enable = true,
+		filetypes = {
+			"html",
+			"javascript",
+			"typescript",
+			"javascriptreact",
+			"typescriptreact",
+			"svelte",
+			"vue",
+			"tsx",
+			"jsx",
+			"rescript",
+			"xml",
+			"php",
+			"markdown",
+			"glimmer",
+			"handlebars",
+			"hbs",
+		},
 	},
 
 	refactor = {
